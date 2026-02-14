f62352ec6ba4e64116a8a493ec7cedc2201f628c
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-13 21:31:45 -0800

    Add while loop to allow program repetition

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // this loop let the user repeat the program based on the input
             string runAgain = "y";
+
+            // repeats as long as the user chooses y
+            while (runAgain == "y")
+            {
+                
+            }
         }
     }
 }
